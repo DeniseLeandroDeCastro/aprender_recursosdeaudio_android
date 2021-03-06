a431aad874dc529a54b8e354023ffca1d48d4660
Denise <dmaria.natal@gmail.com>
2021-03-06 11:24:04 -0300

    pequeno app para aprender recursos de áudio android

---
+++ app/src/main/java/br/edu/ifrn/aprendendoingles/MainActivity.java
@@ -0,0 +1,44 @@
+package br.edu.ifrn.aprendendoingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import br.edu.ifrn.aprendendoingles.fragments.BichosFragment;
+import br.edu.ifrn.aprendendoingles.fragments.NumerosFragment;
+import br.edu.ifrn.aprendendoingles.fragments.VogaisFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configurando a sombra da ActionBar
+        getSupportActionBar().setElevation(0);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                .create()
+        );
+        //Configurando o ViewPager
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/br/edu/ifrn/aprendendoingles/fragments/VogaisFragment.java
@@ -0,0 +1,29 @@
+package br.edu.ifrn.aprendendoingles.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.edu.ifrn.aprendendoingles.R;
+
+public class VogaisFragment extends Fragment {
+
+    public VogaisFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vogais, container, false);
+    }
+}
